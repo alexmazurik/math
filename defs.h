9989feaec086765540389d802ce5632dde1067ea
Alexander Mazurik <almaz@yandex-team.ru>
2014-11-10 00:39:59 +0300

    fix

--- defs.h
+++ defs.h
@@ -9,10 +9,9 @@ typedef unsigned long long ull;
 typedef std::pair<int, int> pii;
 typedef std::pair<ll, ll> pll;
 typedef boost::dynamic_bitset<> Bitset;
-//typedef pii PrimePower;
 
 struct PrimePower {
-    PrimePower(uint32_t prime, uint32_t power) : prime(prime), power(power) {}
+    PrimePower(uint32_t prime, uint8_t power) : prime(prime), power(power) {}
     uint32_t prime;
-    uint32_t power;
+    uint8_t power;
 };
